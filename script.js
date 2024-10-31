44ca01aca0cb7b28f8d38a57ee2d99b52d3f0fa4
Pranjeet Goswami <112185383+parasharr@users.noreply.github.com>
2024-10-31 08:48:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,26 +14,8 @@ if(close){
 }
 
 
-let signupBtn=document.getElementById("signupBtn")
-let signinBtn=document.getElementById("signinBtn")
-let namefield=document.getElementById("namefield")
-let title=document.getElementById("title")
-let passwordBtn=document.getElementById("passwordBtn") 
-            signinBtn.onclick=function(){
-              namefield.style.maxHeight="0";
-              title.innerHTML="Login";
-              signupBtn.classList.add("disable");
-              signinBtn.classList.remove("disable");
-              passwordBtn.style.display="block";
-        
-              
-            }
-            signupBtn.onclick=function(){
-              namefield.style.maxHeight="60px";
-              title.innerHTML="Sign Up";
-              signupBtn.classList.remove("disable");
-              signinBtn.classList.add("disable");
-              passwordBtn.style.display="none";
-        
-              
-            }
+
+
+
+
+
